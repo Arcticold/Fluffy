d9f6a51b7813a5161174de9f9f481b912610857f
Rasmus <Rasmus@192.168.1.134>
2014-05-19 22:22:57 +0300

    New address

--- conf/runtime.conf.php
+++ conf/runtime.conf.php
@@ -1,13 +1,13 @@
 <?php
-
+/*
 define('SITE_URL', 			'http://localhost/fluffy_local/');
 define('MYSQL_HOST', 		'localhost');
 define('MYSQL_USERNAME', 	'root');
 define('MYSQL_PASSWORD', 	'');
 define('MYSQL_DATABASE', 	'proovikas');
+*/
 
-/*
-define('SITE_URL', 			'http://fluffy.site50.net/');
+define('SITE_URL', 			'http://fluffyapp.azurewebsites.net/');
 
 define('MYSQL_HOST', 		'mysql4.000webhost.com');
 
@@ -16,4 +16,3 @@ define('MYSQL_USERNAME', 	'a5322607_fluffy');
 define('MYSQL_PASSWORD', 	'proovikas1');
 
 define('MYSQL_DATABASE', 	'a5322607_fluffy');
-*/
